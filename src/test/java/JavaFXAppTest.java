60b4f8d93fcb22f881b7276039ee04112f981426
Roy Duijndam <Roy.Duijndam1@gmail.com>
2022-04-26 12:04:17 +0200

    make test for computeAdd

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -6,4 +6,15 @@ class JavaFXAppTest {
     void aDummyTest() {
         assertEquals(0,0);
     }
+
+    @Test
+    void add_number_with_number() {
+        int number1 = 7;
+        int number2 = 4;
+
+        int expected = 11;
+        int actual = JavaFXApp.computeAdd(number1, number2);
+
+        assertEquals(expected, actual);
+    }
 }
