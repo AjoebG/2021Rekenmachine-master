2d2fd1337d0f46797c1dde6d18ecad7098b8bd52
AjoebG <101333425+AjoebG@users.noreply.github.com>
2022-04-26 12:00:08 +0200

    Revert "Feature branch divide"

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,14 +5,5 @@ class JavaFXAppTest {
     @Test
     void aDummyTest() {
         assertEquals(0,0);
-    }
-    @Test
-    public void divideTest(){
-        JavaFXApp test = new JavaFXApp();
-
-        int expected = 2;
-        int actual = test.computeDivide(4,2);
-
-        Assertions.assertEquals(expected, actual);
     }
 }
