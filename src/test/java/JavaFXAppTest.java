75c248113f90a28f31c9c44a3809a55cc4eb27c4
Spagetti4 <101333503+Spagetti4@users.noreply.github.com>
2022-04-26 11:57:32 +0200

    divide test added

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -5,5 +6,14 @@ class JavaFXAppTest {
     @Test
     void aDummyTest() {
         assertEquals(0,0);
+    }
+    @Test
+    public void divideTest(){
+        JavaFXApp test = new JavaFXApp();
+
+        int expected = 2;
+        int actual = test.computeDivide(4,2);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
